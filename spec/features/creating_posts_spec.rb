253b88ccef68b55fa26396eb40cc7dedae80fedf
Dani <dani8439@aol.com>
2021-09-30 14:06:51 -0400

    first test passing, can create a post, upload a file and display on show page.

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -4,7 +4,7 @@ feature 'Creating posts' do
     scenario 'can create a post' do 
         visit '/'
         click_link 'New Post'
-        attach file('Image', "spec/files/images/puppy.jpg")
+        attach_file('Image', "spec/files/images/puppy.jpg")
         fill_in 'Caption', with: 'ohhh #puppytime'
         click_button 'Create Post'
         expect(page).to have_content('#puppytime')
